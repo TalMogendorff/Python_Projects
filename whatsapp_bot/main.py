e72e319fe9a7cafd99fe8fa1fb40b5189bae9a5a
Tal Mogendorff <71597455+TalMogendorff@users.noreply.github.com>
2022-01-23 00:55:05 +0200

    Update main.py

--- whatsapp_bot/main.py
+++ whatsapp_bot/main.py
@@ -13,7 +13,7 @@ def job():
     r = requests.get('https://api.chucknorris.io/jokes/random')
     a = json.loads(r.text)
 
-    pywhatkit.sendwhatmsg("+972543117141",f"Hi This is a Chuck Noris Joke: {a['value']}",now.hour,now.minute+1)
+    pywhatkit.sendwhatmsg("+97254-------",f"Hi This is a Chuck Noris Joke: {a['value']}",now.hour,now.minute+1)
 
 job()
 schedule.every().minute.do(job)
